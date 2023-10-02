8ecba15aa4eeb4d0dca0908169b0d2e9bd433e2f
chuba <Chubarov205@gmail.com>
2023-10-02 21:33:49 +0300

    complete15puzzle

--- Lab3/FifteenPuzzle/FifteenPuzzle/FifteenPuzzle.cpp
+++ Lab3/FifteenPuzzle/FifteenPuzzle/FifteenPuzzle.cpp
@@ -1,15 +1,40 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <time.h>
 
 using namespace sf;
 
 int main()
 {
+    srand(time(0));
+
     RenderWindow window(VideoMode(512, 512), "15-Puzzle!");
     window.setFramerateLimit(60);
 
     Texture texture;
-    if (!texture.loadFromFile("C:/Paint/Paint/15.png")) {
+    int a = rand() % 5 + 1;
+    std::string texturePath;
+
+    switch (a)
+    {
+    case 1:
+        texturePath = "C:/Paint/Paint/Pro/1.png";
+        break;
+    case 2:
+        texturePath = "C:/Paint/Paint/Pro/2.png";
+        break;
+    case 3:
+        texturePath = "C:/Paint/Paint/Pro/3.png";
+        break;
+    case 4:
+        texturePath = "C:/Paint/Paint/Pro/4.png";
+        break;
+    case 5:
+        texturePath = "C:/Paint/Paint/Pro/5.png";
+        break;
+    }
+
+    if (!texture.loadFromFile(texturePath)) {
         // Обработка ошибки загрузки текстуры
         std::cerr << "Ошибка загрузки текстуры" << std::endl;
     }
@@ -26,87 +51,96 @@ int main()
             grid[i + 1][j + 1] = n;
         }
 
+    float speed = 16; // Скорость анимации
+    Clock clock;
+
     // Главный цикл приложения: выполняется, пока открыто окно
     while (window.isOpen())
     {
+        float deltaTime = clock.restart().asSeconds(); // Время прошедшее с последнего кадра
+
         // Обрабатываем события в цикле
         Event event;
-        while (window.isOpen())
+        while (window.pollEvent(event))
         {
-            // Обрабатываем события в цикле
-            Event event;
-            while (window.pollEvent(event))
+            // Пользователь нажал на «крестик» и хочет закрыть окно?
+            if (event.type == Event::Closed)
             {
-                // Пользователь нажал на «крестик» и хочет закрыть окно?
-                if (event.type == Event::Closed)
-                {
-                    // тогда закрываем его
-                    window.close();
-                }
+                // тогда закрываем его
+                window.close();
+            }
 
-                // Проверяем событие нажатия кнопки мыши
-                if (event.type == Event::MouseButtonPressed)
+            // Проверяем событие нажатия кнопки мыши
+            if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.key.code == Mouse::Left)
                 {
-                    if (event.key.code == Mouse::Left)
+                    Vector2i pos = Mouse::getPosition(window);
+
+                    int x = pos.x / w + 1;
+                    int y = pos.y / w + 1;
+
+                    int dx = 0;
+                    int dy = 0;
+
+                    int b;
+                    if (a != 2 && a != 5)
+                        b = 16;
+                    else if (a == 2 || a == 5)
+                        b = 4;
+
+                    if (grid[x + 1][y] == b)
                     {
-                        Vector2i pos = Mouse::getPosition(window);
+                        dx = 1;
+                    }
+                    else if (grid[x - 1][y] == b)
+                    {
+                        dx = -1;
+                    }
+                    else if (grid[x][y - 1] == b)
+                    {
+                        dy = -1;
+                    }
+                    else if (grid[x][y + 1] == b)
+                    {
+                        dy = 1;
+                    }
 
-                        int x = pos.x / w + 1;
-                        int y = pos.y / w + 1;
+                    if (dx != 0 || dy != 0)
+                    {
+                        n = grid[x][y];
+                        grid[x][y] = b;
+                        grid[x + dx][y + dy] = n;
 
-                        int dx = 0;
-                        int dy = 0;
-
-                        if (grid[x + 1][y] == 16)
-                        {
-                            dx = 1;
-                        }
-                        else if (grid[x - 1][y] == 16)
-                        {
-                            dx = -1;
-                        }
-                        else if (grid[x][y - 1] == 16)
-                        {
-                            dy = -1;
-                        }
-                        else if (grid[x][y + 1] == 16)
-                        {
-                            dy = 1;
-                        }
-
-                        if (dx != 0 || dy != 0)
-                        {
-                            n = grid[x][y];
-                            grid[x][y] = 16;
-                            grid[x + dx][y + dy] = n;
-                        }
-
-                        sprite[16].move(-dx * w, -dy * w);
+                        // Анимация движения
+                        sprite[b].move(-dx * w, -dy * w);
                         float speed = 16;
                         for (int i = 0; i < w; i += speed) {
                             sprite[n].move(speed * dx, speed * dy);
-                            window.draw(sprite[16]);
+                            window.draw(sprite[b]);
                             window.draw(sprite[n]);
                             window.display();
                         }
+                        
                     }
 
                 }
             }
-
-            // Устанавливаем цвет фона - белый
-            window.clear(Color::White);
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++) {
-                    n = grid[i + 1][j + 1];
-                    sprite[n].setPosition(i * w, j * w);
-                    // Отрисовка спрайта
-                    window.draw(sprite[n]);
-                }
-
-            window.display();
         }
 
+        // Устанавливаем цвет фона - белый
+        window.clear(Color::White);
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) {
+                n = grid[i + 1][j + 1];
+                sprite[n].setPosition(i * w, j * w);
+                // Отрисовка спрайта
+                window.draw(sprite[n]);
+            }
+
+        // Отрисовка окна
+        window.display();
     }
+
     return 0;
 }
